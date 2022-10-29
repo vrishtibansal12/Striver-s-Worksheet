21ba4cbb7c433b02fe79552f3bad3ea21bb73dd3
Vrishti Bansal <vrishti2012@gmail.com>
2022-10-29 22:47:39 +0530

    Add files via upload

---
+++ Count Inversions.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+int merge(long long int arr[],long long int temp[],int left,int mid,int right)
+{
+    int inv_count=0;
+    int i = left;
+    int j = mid;
+    int k = left;
+    while((i <= mid-1) && (j <= right)){
+        if(arr[i] <= arr[j]){
+            temp[k++] = arr[i++];
+        }
+        else
+        {
+            temp[k++] = arr[j++];
+            inv_count = inv_count + (mid - i);
+        }
+    }
+
+    while(i <= mid - 1)
+        temp[k++] = arr[i++];
+
+    while(j <= right)
+        temp[k++] = arr[j++];
+
+    for(i = left ; i <= right ; i++)
+        arr[i] = temp[i];
+    
+    return inv_count;
+}
+
+int merge_Sort(long long arr[],long long int temp[],int left,int right)
+{
+    int mid,inv_count = 0;
+    if(right > left)
+    {
+        mid = (left + right)/2;
+
+        inv_count += merge_Sort(arr,temp,left,mid);
+        inv_count += merge_Sort(arr,temp,mid+1,right);
+
+        inv_count += merge(arr,temp,left,mid+1,right);
+    }
+    return inv_count;
+}
+long long getInversions(long long *arr, int n){
+    long long int temp[n];
+    return merge_Sort(arr,temp,0,n-1);
+}
